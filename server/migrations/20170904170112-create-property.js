833a734f190ffe2eafff98a143b75d0cdba256dd
Abi Travers <atravers4007@gmail.com>
2017-09-04 17:44:30 +0100

    Property table up and working

--- server/migrations/20170904170112-create-property.js
+++ server/migrations/20170904170112-create-property.js
@@ -24,7 +24,7 @@ module.exports = {
         type: Sequelize.FLOAT
       },
       dates: {
-        type: Sequelize.RANGE(DATE)
+        type: Sequelize.RANGE(Sequelize.DATE)
       },
       createdAt: {
         allowNull: false,
