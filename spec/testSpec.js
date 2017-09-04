a719f035b34f61242fa7f4bb136f0741cdd9608c
Will Porter <willjsporter@hotmail.co.uk>
2017-09-04 14:53:48 +0100

    new branch and jasmine stuff

---
+++ spec/testSpec.js
@@ -0,0 +1,7 @@
+var Browser = require("zombie");
+var url = "http://work.krasimirtsonev.com/git/blog-posts/TestingWithZombieJS/site/";
+var browser = new Browser();
+
+describe("testing with zombie", function() {
+
+});
