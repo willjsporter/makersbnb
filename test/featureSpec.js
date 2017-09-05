ab1f715c89c2ebeabd0e2b5838a328636ff1cf4e
Lizzie Hard <elizabeth_hard@hotmail.co.uk>
2017-09-05 12:09:28 +0100

    created new property list page

---
+++ test/featureSpec.js
@@ -0,0 +1,46 @@
+const Browser = require('zombie');
+
+// We're going to make requests to http://example.com/signup
+// Which will be routed to our test server localhost:3000
+Browser.localhost('localhost', 3000);
+
+describe('Makersbnb app', function() {
+
+  const browser = new Browser();
+
+  describe('submits form', function() {
+    before(function(done) {
+      browser.visit('/', done);
+    });
+
+    // before(function(done) {
+    //   browser
+    //     .fill('email',    'zombie@underworld.dead')
+    //     .fill('password', 'eat-the-living')
+    //     .pressButton('Sign Me Up!', done);
+    // });
+
+    it('should be successful', function() {
+      browser.assert.success();
+    });
+
+    it('should show text', function() {
+      browser.assert.text('body', "Hello World!");
+    });
+  });
+
+  describe('submits form', function() {
+    before(function(done) {
+      browser.visit('/propertylist', done);
+    });
+
+    it('should be successful', function() {
+      browser.assert.success();
+    });
+
+    it('should have a list of properties', function() {
+      browser.assert.text('body', 'Property List');
+    })
+  });
+
+});
