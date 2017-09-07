668cf68416773711b8b13cdd3030ebfeb2eb5a8a
Lizzie Hard <elizabeth_hard@hotmail.co.uk>
2017-09-07 11:09:12 +0100

    updated tests

--- test/featureSpec.js
+++ test/featureSpec.js
@@ -34,62 +34,62 @@ describe('Makersbnb app', function() {
     });
   });
 
-  xdescribe('Log in', function() {
-      before(function(done) {
-        browser.visit('/', function(){
+  describe('Log in', function() {
+    before(function(done) {
+      browser.visit('/firstpage', function(){
         browser.fill('username', 'Muffin')
         browser.fill('password', 'Secrete')
-        browser.pressButton("Welcome Back!",done);
-      });
-
-      it("has a log in form which can be filled in", function() {
-      });
-    });
-  });
-
-
-  describe('property list should load', function() {
-    before(function(done) {
-      browser.visit('/propertylist', done);
-    });
-
-    it('property list should have a title of MakersBnb', function() {
-      browser.assert.text('title', 'MakersBnB');
-    });
-
-  });
-
-  describe('add property should load', function() {
-    before(function(done) {
-      browser.visit('/addproperty', done);
-    });
-
-    it('should have a title of MakersBnb', function() {
-      browser.assert.text('title', 'MakersBnB');
-    });
-  });
-
-
-
-  describe('add property should add a property', function() {
-
-    before(function(done) {
-      browser.visit('/addproperty', function() {
-        browser.fill('name', 'Makerz')
-        browser.fill('location', '123 Makers Academy St')
-        browser.fill('description', 'misery-land')
-        browser.fill('price', '£90 per night trololololol')
-        browser.pressButton('Add!',done);
+        browser.pressButton("Login",done);
       });
     });
 
-    it('should have property list on page', function(){
-      browser.assert.text('title', 'MakersBnB');
+    it("has a log in form which can be filled in", function() {
+      browser.assert.text('h1', 'Welcome to Legend BnB Muffin');
+    });;
+
+
+    describe('property list should load', function() {
+      before(function(done) {
+        browser.visit('/propertylist', done);
+      });
+
+      it('property list should have a title of MakersBnb', function() {
+        browser.assert.text('title', 'MakersBnB');
+      });
+
     });
 
-    it('should have added the property to the list', function(){
-      browser.assert.text('body', '123 Makers Academy St');
+    describe('add property should load', function() {
+      before(function(done) {
+        browser.visit('/addproperty', done);
+      });
+
+      it('should have a title of MakersBnb', function() {
+        browser.assert.text('title', 'MakersBnB');
+      });
     });
 
+
+
+    describe('add property should add a property', function() {
+
+      before(function(done) {
+        browser.visit('/addproperty', function() {
+          browser.fill('name', 'Makerz')
+          browser.fill('location', '123 Makers Academy St')
+          browser.fill('description', 'misery-land')
+          browser.fill('price', '£90 per night trololololol')
+          browser.pressButton('Add!',done);
+        });
+      });
+
+      it('should have property list on page', function(){
+        browser.assert.text('title', 'MakersBnB');
+      });
+
+      it('should have added the property to the list', function(){
+        browser.assert.text('body', '123 Makers Academy St');
+      });
+    });
   });
 });
